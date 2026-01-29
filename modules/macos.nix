e352b6cb73074c947569559e2287e018657ae788
Miha Oblišar <miha.oblishar@gmail.com>
2026-01-29 17:05:12 +0100

    fixed modules

--- modules/macos.nix
+++ modules/macos.nix
@@ -1,22 +1,45 @@
 { ... }: {
   security.pam.services.sudo_local.touchIdAuth = true;
 
-
   system.defaults = {
+    finder = {
+      FXPreferredViewStyle = "clmv"; 
+      _FXShowPosixPathInTitle = true;
+    };
+
     dock = {
-      autohide = true;
+      autohide = false;
       show-recents = false;
+      static-only = false;
       persistent-apps = [
         "/System/Applications/Messages.app"
-        "/System/Library/CoreServices/Finder.app"
         "/System/Applications/App Store.app"
       ];
+      persistent-others = [];
     };
-    
-    # Use CustomUserPreferences for keys not explicitly in nix-darwin
+
     CustomUserPreferences = {
       "com.apple.finder" = {
         ShowRecentTags = false;
+        FXPreferredGroupBy = "Kind";
+      };
+      "com.apple.dock" = {
+        show-recents = false;
+        "recent-apps" = [];
+        "recent-documents" = [];
+        "recent-servers" = [];
+      };
+      "com.apple.coreservices.userappbindings" = {
+        LSHandlers = [
+          {
+            LSHandlerURLScheme = "http";
+            LSHandlerRoleAll = "com.vivaldi.Vivaldi";
+          }
+          {
+            LSHandlerURLScheme = "https";
+            LSHandlerRoleAll = "com.vivaldi.Vivaldi";
+          }
+        ];
       };
     };
 
