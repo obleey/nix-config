d38984cfaae394ce4b0b0a2e582e0822059deb63
Miha Oblišar <miha.oblishar@gmail.com>
2026-01-29 17:41:09 +0100

    code to dock

--- modules/macos.nix
+++ modules/macos.nix
@@ -13,7 +13,7 @@
       persistent-apps = [
         "/System/Applications/Messages.app"
         "/System/Applications/App Store.app"
-        "/System/Applications/Visual Studio Code.app"
+        "/System/Applications/Nix Apps/Visual Studio Code.app"
       ];
       persistent-others = [];
     };
