b91118748834aee0b06f280c9f9353f77a802e1a
Miha Oblišar <miha.oblishar@gmail.com>
2026-01-29 19:20:19 +0100

    add ssh agent

--- modules/home.nix
+++ modules/home.nix
@@ -154,4 +154,12 @@
       nix-conf = "code ~/nix-darwin-config";
     };
   };
+
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
+      Host *
+        IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+    '';
+  };
 }
