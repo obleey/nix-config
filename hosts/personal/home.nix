ed8cd66b35196c933ea15a0dbff386cb1d529dfa
Miha Oblišar <miha.oblishar@gmail.com>
2026-01-30 15:58:21 +0100

    reformat

--- hosts/personal/home.nix
+++ hosts/personal/home.nix
@@ -1,4 +1,10 @@
-{ pkgs, user, hostname, ... }: {
+{
+  pkgs,
+  user,
+  hostname,
+  ...
+}:
+{
   imports = [ ../../systems/aarch64-darwin/home-base.nix ];
 
   programs.git = {
@@ -13,14 +19,14 @@
   };
 
   programs.ssh = {
-      enable = true;
-      enableDefaultConfig = false;
-      matchBlocks = {
-        "*" = {
-          extraOptions = {
-            IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
-          };
+    enable = true;
+    enableDefaultConfig = false;
+    matchBlocks = {
+      "*" = {
+        extraOptions = {
+          IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
         };
       };
     };
+  };
 }
